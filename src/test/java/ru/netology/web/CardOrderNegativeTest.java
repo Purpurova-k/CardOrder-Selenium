b355ed0be58d4f84051880d0c9204ffe7c34acdb
Purpurova-k <katrin.purpurova@gmail.com>
2022-02-07 15:53:41 +0300

    Corrections added

--- src/test/java/ru/netology/web/CardOrderNegativeTest.java
+++ src/test/java/ru/netology/web/CardOrderNegativeTest.java
@@ -17,12 +17,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class CardOrderNegativeTest {
     private WebDriver driver;
 
-    WebElement nameField = driver.findElement(By.cssSelector("[data-test-id=name] input"));
-    WebElement phoneField = driver.findElement(By.cssSelector("[data-test-id=phone] input"));
-    WebElement checkbox = driver.findElement(By.cssSelector("[data-test-id=agreement] span"));
-    WebElement button = driver.findElement(By.cssSelector("button"));
-
-
+    private WebElement nameField;
+    private WebElement phoneField;
+    private WebElement checkbox;
+    private WebElement button;
 
     @BeforeAll
     public static void setUpAll() {
@@ -38,7 +36,11 @@ public class CardOrderNegativeTest {
 
         driver = new ChromeDriver(options);
 
-        driver.get("http://localhost:9999/");;
+        driver.get("http://localhost:9999/");
+        nameField = driver.findElement(By.cssSelector("[data-test-id=name] input"));
+        phoneField = driver.findElement(By.cssSelector("[data-test-id=phone] input"));
+        checkbox = driver.findElement(By.cssSelector("[data-test-id=agreement] span"));
+        button = driver.findElement(By.cssSelector("button"));
     }
 
     @AfterEach
@@ -62,95 +64,95 @@ public class CardOrderNegativeTest {
     }
 
 
-//    @Test
-//    public void shouldNotSendFormInvalidNameFieldWithNumbers() {
-//        driver.findElement(By.cssSelector(nameField)).sendKeys("Иванов Иван999");
-//        driver.findElement(By.cssSelector(phoneField)).sendKeys("+79998887766");
-//        driver.findElement(By.cssSelector(checkbox)).click();
-//        driver.findElement(By.cssSelector(button)).click();
-//
-//        String expectedText = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
-//        String actualText = driver.findElement(By.cssSelector("[data-test-id=name] span.input__sub")).getText().trim();
-//
-//        assertEquals(expectedText, actualText);
-//    }
-//
-//
-//    @Test
-//    public void shouldNotSendFormInvalidNameFieldEmpty() {
-//        driver.findElement(By.cssSelector(phoneField)).sendKeys("+79998887766");
-//        driver.findElement(By.cssSelector(checkbox)).click();
-//        driver.findElement(By.cssSelector(button)).click();
-//
-//        String expectedText = "Поле обязательно для заполнения";
-//        String actualText = driver.findElement(By.cssSelector("[data-test-id=name] span.input__sub")).getText().trim();
-//
-//        assertEquals(expectedText, actualText);
-//    }
-//
-//
-//    @Test
-//    public void shouldNotSendFormInvalidPhoneFieldLetters() {
-//        driver.findElement(By.cssSelector(nameField)).sendKeys("Иванов Иван");
-//        driver.findElement(By.cssSelector(phoneField)).sendKeys("телефон");
-//        driver.findElement(By.cssSelector(checkbox)).click();
-//        driver.findElement(By.cssSelector(button)).click();
-//
-//        String expectedText = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
-//        String actualText = driver.findElement(By.cssSelector("[data-test-id=phone] span.input__sub")).getText().trim();
-//
-//        assertEquals(expectedText, actualText);
-//    }
-//
-//
-//    @Test
-//    public void shouldNotSendFormInvalidPhoneFieldFewNumbers() {
-//        driver.findElement(By.cssSelector(nameField)).sendKeys("Иванов Иван");
-//        driver.findElement(By.cssSelector(phoneField)).sendKeys("+7999");
-//        driver.findElement(By.cssSelector(checkbox)).click();
-//        driver.findElement(By.cssSelector(button)).click();
-//
-//        String expectedText = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
-//        String actualText = driver.findElement(By.cssSelector("[data-test-id=phone] span.input__sub")).getText().trim();
-//
-//        assertEquals(expectedText, actualText);
-//    }
-//
-//
-//    @Test
-//    public void shouldNotSendFormInvalidPhoneFieldWithoutPlus() {
-//        driver.findElement(By.cssSelector(nameField)).sendKeys("Иванов Иван");
-//        driver.findElement(By.cssSelector(phoneField)).sendKeys("79998887766");
-//        driver.findElement(By.cssSelector(checkbox)).click();
-//        driver.findElement(By.cssSelector(button)).click();
-//
-//        String expectedText = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
-//        String actualText = driver.findElement(By.cssSelector("[data-test-id=phone] span.input__sub")).getText().trim();
-//
-//        assertEquals(expectedText, actualText);
-//    }
-//
-//
-//    @Test
-//    public void shouldNotSendFormInvalidPhoneFieldEmpty() {
-//        driver.findElement(By.cssSelector(nameField)).sendKeys("Иванов Иван");
-//        driver.findElement(By.cssSelector(checkbox)).click();
-//        driver.findElement(By.cssSelector(button)).click();
-//
-//        String expectedText = "Поле обязательно для заполнения";
-//        String actualText = driver.findElement(By.cssSelector("[data-test-id=phone] span.input__sub")).getText().trim();
-//
-//        assertEquals(expectedText, actualText);
-//    }
-//
-//
-//    @Test
-//    public void shouldNotSendFormUncheckedCheckbox() {
-//        driver.findElement(By.cssSelector(nameField)).sendKeys("Иванов Иван");
-//        driver.findElement(By.cssSelector(phoneField)).sendKeys("+79998887766");
-//        driver.findElement(By.cssSelector(button)).click();
-//
-//        String checkboxInvalid = driver.findElement(By.cssSelector("[data-test-id=agreement].checkbox")).getAttribute("className");
-//        assertTrue(checkboxInvalid.contains("input_invalid"));
-//    }
+    @Test
+    public void shouldNotSendFormInvalidNameFieldWithNumbers() {
+        nameField.sendKeys("Иванов Иван999");
+        phoneField.sendKeys("+79998887766");
+        checkbox.click();
+        button.click();
+
+        String expectedText = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        String actualText = driver.findElement(By.cssSelector("[data-test-id=name] span.input__sub")).getText().trim();
+
+        assertEquals(expectedText, actualText);
+    }
+
+
+    @Test
+    public void shouldNotSendFormInvalidNameFieldEmpty() {
+        phoneField.sendKeys("+79998887766");
+        checkbox.click();
+        button.click();
+
+        String expectedText = "Поле обязательно для заполнения";
+        String actualText = driver.findElement(By.cssSelector("[data-test-id=name] span.input__sub")).getText().trim();
+
+        assertEquals(expectedText, actualText);
+    }
+
+
+    @Test
+    public void shouldNotSendFormInvalidPhoneFieldLetters() {
+        nameField.sendKeys("Иванов Иван");
+        phoneField.sendKeys("телефон");
+        checkbox.click();
+        button.click();
+
+        String expectedText = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        String actualText = driver.findElement(By.cssSelector("[data-test-id=phone] span.input__sub")).getText().trim();
+
+        assertEquals(expectedText, actualText);
+    }
+
+
+    @Test
+    public void shouldNotSendFormInvalidPhoneFieldFewNumbers() {
+        nameField.sendKeys("Иванов Иван");
+        phoneField.sendKeys("+7999");
+        checkbox.click();
+        button.click();
+
+        String expectedText = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        String actualText = driver.findElement(By.cssSelector("[data-test-id=phone] span.input__sub")).getText().trim();
+
+        assertEquals(expectedText, actualText);
+    }
+
+
+    @Test
+    public void shouldNotSendFormInvalidPhoneFieldWithoutPlus() {
+        nameField.sendKeys("Иванов Иван");
+        phoneField.sendKeys("79998887766");
+        checkbox.click();
+        button.click();
+
+        String expectedText = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        String actualText = driver.findElement(By.cssSelector("[data-test-id=phone] span.input__sub")).getText().trim();
+
+        assertEquals(expectedText, actualText);
+    }
+
+
+    @Test
+    public void shouldNotSendFormInvalidPhoneFieldEmpty() {
+        nameField.sendKeys("Иванов Иван");
+        checkbox.click();
+        button.click();
+
+        String expectedText = "Поле обязательно для заполнения";
+        String actualText = driver.findElement(By.cssSelector("[data-test-id=phone] span.input__sub")).getText().trim();
+
+        assertEquals(expectedText, actualText);
+    }
+
+
+    @Test
+    public void shouldNotSendFormUncheckedCheckbox() {
+        nameField.sendKeys("Иванов Иван");
+        phoneField.sendKeys("+79998887766");
+        button.click();
+
+        String checkboxInvalid = driver.findElement(By.cssSelector("[data-test-id=agreement].checkbox")).getAttribute("className");
+        assertTrue(checkboxInvalid.contains("input_invalid"));
+    }
 }
